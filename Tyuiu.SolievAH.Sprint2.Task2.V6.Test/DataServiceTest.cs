895e872a126fa81f296aab6ad8b594439119e57c
Tyuiu.SolievAH@outlook.com <katagawa.k@gmail.com>
2023-10-15 15:51:34 +0500

    Task2

---
+++ Tyuiu.SolievAH.Sprint2.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SolievAH.Sprint2.Task2.V6.Lib;
+
+namespace Tyuiu.SolievAH.Sprint2.Task2.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 8, y = 9;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
